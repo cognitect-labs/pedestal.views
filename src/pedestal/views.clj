327a4e8084e289ff95b9794531b7e4658090b8a6
Michael Nygard <mtnygard@gmail.com>
2017-01-27 09:05:36 -0600

    initial import

---
+++ src/pedestal/views.clj
@@ -0,0 +1,6 @@
+(ns pedestal.views)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pedestal/views_test.clj
@@ -0,0 +1,7 @@
+(ns pedestal.views-test
+  (:require [clojure.test :refer :all]
+            [pedestal.views :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
